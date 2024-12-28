57013fe9e56def0b995efb5f7745d507d6eba042
Soubhik <soubhiksarkar226@gmail.com>
2024-12-28 19:28:50 +0530

    adding a button

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const AC=document.querySelector("#AC");
 const input = document.querySelector("#input");
 const backspace=document.querySelector("#backspace");
 const btn_=document.querySelector("#btn_");
+const button = document.querySelector('#button');
+const text = document.querySelector('.text');
 
 
 function appendToDisplay(e){
@@ -21,3 +23,14 @@ function calculate() {
 function clear_1(){
     input.value=input.value.slice(0,-1);
 }
+function move(){
+        if(text.classList.contains('circle')){
+            text.classList.remove('circle');
+            text.classList.add('circle_1');
+    }
+else{
+    text.classList.add('circle');
+    text.classList.remove('circle_1');
+}
+};
+
